63bca910fc8997a3ac33ff8d5a9e38d5ad9baad6
Drip Wolf <dripwolf303@gmail.com>
2019-10-01 20:21:01 -0600

    created folders and variables

---
+++ questions.js
@@ -0,0 +1,21 @@
+var questions = [{
+    title: "Which of the folloing is NOT a common data type:",
+    choices: ["string", "boolean", "alert", "number"],
+    answer: "alerts"
+},
+  {
+      title: "Which data type is associated with true or false:",
+      choices: ["number", "string", "boolean", "null" ],
+      answer: "boolean"
+  },
+  {
+      title: "JavaScript was initially created in how many days:",
+      choices: ["30 days", "10 days", "377 days", "60 days"],
+      answer: "10 days"
+  },
+  {
+      title: "Which of the following is NOT used in JavaScript:",
+      choices: ["function()", "boolean", "<p>","var"],
+      answer: "<p>"
+  },
+];
